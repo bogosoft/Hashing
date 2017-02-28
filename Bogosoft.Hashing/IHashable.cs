1076485f65d1b526469485d43eca3061c2322c93
Clayton Roth <croth0627@yahoo.com>
2017-02-27 22:30:11 -0800

    initial code commit

---
+++ Bogosoft.Hashing/IHashable.cs
@@ -0,0 +1,16 @@
+﻿namespace Bogosoft.Hashing
+{
+    /// <summary>
+    /// Indicates that an implementation is capable of generating a unique sequence of bytes.
+    /// </summary>
+    public interface IHashable
+    {
+        /// <summary>
+        /// Generate a sequence of bytes that represent the unique internal state of the current object.
+        /// </summary>
+        /// <returns>
+        /// A variable length sequence of bytes unique to the state of the current object.
+        /// </returns>
+        byte[] GetBytes();
+    }
+}
